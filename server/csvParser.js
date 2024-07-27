453a3ad9aa1cb886e7e66ad6652743d34b9b2f49
brahyerr <head@phams.page>
2024-07-27 19:40:28 -0400

    csvParser.json: write parsed file to JSON

--- server/csvParser.js
+++ server/csvParser.js
@@ -1,36 +1,42 @@
 const fs = require('fs').promises;
 const Papa = require('papaparse');
 
+const PARSED_NAME = 'parsed.json';
+
+// check if user permission obtained to store data
 // parses the csv data file
+
 const parseFile = async function () {
 
     // read csv file and reformat into json
-        try {
-            const data = await fs.readFile('data.csv', 'utf-8');
-            console.log("we get here");
+    try {
+        const data = await fs.readFile('data.csv', 'utf-8');
+        console.log("we get here");
 
-            const results = Papa.parse(data,
-                {   
-                    header: true,
-                    skipEmptyLines: true
-                }
-            )
+        const results = Papa.parse(data, {
+	    header: true,
+	    skipEmptyLines: true
+	})
 
-            // return json file
-            return results.data;
+        // return json file
+        return results.data;
 
-        } catch (err) {
-            console.log("error reading file");
-            return null;
-        }
+    } catch (err) {
+        console.log("error reading file");
+        return null;
+    }
 
-    };
+};
 
 parseFile().then(
     // data => console.log(data)
     data => {
-	let filtered = data.filter(element => element.name == "AudioEye Inc")?.length == 1 ? true : false;
-	console.log(filtered);
+	let nameExists = data.filter(element => element.name == "AudioEye Inc")?.length == 1 ? true : false;
+	console.log(nameExists);
+	fs.writeFile('parsed.json', JSON.stringify(data), function (err) {
+	    if (err) throw err;
+	    console.log('saved');
+	});
     }
 
 ).catch(
