72a0a830c3d7fb6776de59fa0c10999004312f51
The1And0n1y <hashimmahdi123@gmail.com>
2024-07-28 00:06:42 -0400

    Added functionality

--- extension/scripts/matches.js
+++ extension/scripts/matches.js
@@ -1,21 +1,31 @@
 const style = document.createElement("style");
+
 style.innerHTML = ".hide {display: none;}"
     + ".display {"
     + " position: absolute;"
-    + " border: 2px solid red;"
-    + " background-color: white;"
+    + " border: 2px solid white;"
+    + " background-color: green;"
     + "}";
 
 document.head.insertBefore(style, null);
 
 const tagInfo = document.createElement("span");
-const AllCompanyTitle = document.body.getElementsByClassName(".VuuXrf");
+//const AllLinks = document.body.getElementsByTagName("h3");
+const AllComp = document.body.getElementsByClassName("VuuXrf");
 
-for (let i = 0; i < AllCompanyTitle.length; i++) {
-    let Company = AllCompanyTitle[i];
-
+for (let i = 0; i < AllComp.length; i++) {
+    //let Link = AllLinks[i];
+    let Company = AllComp[i];
     Company.addEventListener("mouseenter", (e) => {
-        let outerHTML = Company.outerHTML;
-        outerHTML = outerHTML.substring(0,200)
+        tagInfo.innerHTML = Company.innerHTML;
+        tagInfo.className = "display";
+        tagInfo.style.left = e.pageX + "px";
+        tagInfo.style.top = e.pageY + "px";
     });
+    console.log(Company.innerHTML);
 }
+
+
+
+tagInfo.className = "hide";
+document.body.insertBefore(tagInfo, null);
