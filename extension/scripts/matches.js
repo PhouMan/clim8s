42d591bf6a3dfedd866a9b40a200fcddaff3e7e7
brahyerr <head@phams.page>
2024-07-27 23:47:19 -0400

    Another one

--- extension/scripts/matches.js
+++ extension/scripts/matches.js
@@ -1,3 +1,4 @@
+const link = document.querySelector("h3");
 const style = document.createElement("style");
 style.innerHTML = ".hide {display: none;}"
     + ".display {"
@@ -9,13 +10,17 @@ style.innerHTML = ".hide {display: none;}"
 document.head.insertBefore(style, null);
 
 const tagInfo = document.createElement("span");
-const AllCompanyTitle = document.body.getElementsByClassName(".VuuXrf");
+link.addEventListener("mouseenter", (e) => {
+    console.log("a");
+});
+// const AllCompanyTitle = document.body.getElementsByClassName("VuuXrf");
 
-for (let i = 0; i < AllCompanyTitle.length; i++) {
-    let Company = AllCompanyTitle[i];
+// for (let i = 0; i < AllCompanyTitle.length; i++) {
+//     let Company = AllCompanyTitle[i];
 
-    Company.addEventListener("mouseenter", (e) => {
-        let outerHTML = Company.outerHTML;
-        outerHTML = outerHTML.substring(0,200)
-    });
-}
+//     Company.addEventListener("mouseenter", (e) => {
+//         let outerHTML = Company.outerHTML;
+//         outerHTML = outerHTML.substring(0,200)
+// 	console.log("a");
+//     });
+// }
